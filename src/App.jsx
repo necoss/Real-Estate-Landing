f4ae54164c0b45d7fa469adea2f1fbe54f63448e
trexi <vartypeez@gmail.com>
2025-04-17 13:21:26 +0300

    Imported Navbar and Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div className='text-4xl'>App</div>
+    <div className='w-full overflow-hidden'>
+      <Header />
+    </div>
   )
 }
 
