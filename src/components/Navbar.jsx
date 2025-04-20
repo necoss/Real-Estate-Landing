cdc4737aecab70f44b97312b4c235847b4e72c05
trexi <vartypeez@gmail.com>
2025-04-20 16:57:50 +0300

    Fixed Show mobile menu function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 		return () => {
 			document.body.style.overflow = 'auto'		
 		}
-	}, [showMobileMenu])
+	}, [showMobileMenu]) 
 
   return (
     <div className="absolute top-0 left-0 w-full z-10">
